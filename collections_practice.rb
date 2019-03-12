3755cb37108b5aa7f35207fe7403dd8035690056
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-12 20:52:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,11 @@
 def sort_array_asc(array)
   array = [22, 15, 45, 6]
-  array.sort do 
+  array.sort do
     if a == b
       0
-    elsif a < b 
+    elsif a < b
       -1
-    elsif a > b 
+    elsif a > b
       1
     end
   end
