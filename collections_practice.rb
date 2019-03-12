0924935c1d03dba1b689ed5b34d5de2b63cd4a59
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-12 20:59:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,6 +8,14 @@ end
 
 def sort_array_desc(array)
   array = [22, 15, 45, 6]
-  array.sort do
-
+  array.sort do |b,a|
+    if a !<=> b 
+    end
   end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
